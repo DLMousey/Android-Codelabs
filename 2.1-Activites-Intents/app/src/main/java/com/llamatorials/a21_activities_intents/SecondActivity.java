87537a01cc5baac6758385fd2dddc946fe320621
DLMousey <tom@llamatorials.com>
2018-09-20 21:29:26 +0100

    Complete CodeLab 2.2 - Lifecycle & State

--- 2.1-Activites-Intents/app/src/main/java/com/llamatorials/a21_activities_intents/SecondActivity.java
+++ 2.1-Activites-Intents/app/src/main/java/com/llamatorials/a21_activities_intents/SecondActivity.java
@@ -3,12 +3,14 @@ package com.llamatorials.a21_activities_intents;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
     public static final String EXTRA_REPLY = "com.llamatorials.a21_activities_intents.extra.REPLY";
 
     private EditText mReply;
@@ -25,6 +27,13 @@ public class SecondActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.text_message);
 
         textView.setText(message);
+        Log.d(LOG_TAG, "onCreate");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
     }
 
     public void returnReply(View view) {
@@ -32,6 +41,8 @@ public class SecondActivity extends AppCompatActivity {
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK, replyIntent);
+
+        Log.d(LOG_TAG, "End SecondActivity");
         finish();
     }
 }
